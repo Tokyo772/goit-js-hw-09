58d52400ef820156379e1daa47aedd8d8bd83b11
Tokyo772 <merokyry33@gmail.com>
2024-04-05 12:41:54 +0200

    add trim

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -14,8 +14,8 @@ loadForm();
 
 const sendToLocalStorage = () => {
   const formData = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
+    email: form.elements.email.value.trim(),
+    message: form.elements.message.value.trim(),
   };
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
 };
@@ -24,8 +24,8 @@ form.addEventListener('input', sendToLocalStorage);
 const submitForm = event => {
   event.preventDefault();
 
-  const formEmail = form.elements.email.value;
-  const formMessage = form.elements.message.value;
+  const formEmail = form.elements.email.value.trim();
+  const formMessage = form.elements.message.value.trim();
   if (formEmail && formMessage) {
     const savedData = localStorage.getItem(localStorageKey);
 
